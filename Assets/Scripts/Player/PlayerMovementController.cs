976ff0301c767c585505baa59b524a193d3f81c3
Dawid Harendarz <dharendarz2@gmail.com>
2025-01-24 19:33:14 +0100

    Create new class for ground checking

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -10,34 +10,17 @@ namespace RehvidGames.Player
         [SerializeField] private Rigidbody rb;
         [SerializeField] private SpriteRenderer characterSprite;
         [SerializeField] private Animator animator;
+        [SerializeField] private GroundChecker groundChecker;
         
         [Header("Properties")] 
         [SerializeField] private float speed = 5f;
         [SerializeField] private float jumpForce = 5f;
         [SerializeField] private float dashForce = 20f;
         [SerializeField] private float fallSpeedMultiplier = 3f;
-        [SerializeField] private LayerMask groundLayer;
-        
+    
         private Vector2 inputMovement;
         private bool isJumping;
         private bool onGround;
-        
-        private void Start()
-        { 
-            InitializeOnGround();
-        }
-
-        private void InitializeOnGround()
-        {
-            if (TryGetComponent(out SphereCollider sphereCollider))
-            {
-                onGround = Physics.CheckSphere(sphereCollider.center, sphereCollider.radius, groundLayer);
-            }
-            else
-            {
-                Debug.LogWarning("No SphereCollider found for ground check. Player may not detect collisions with the ground.");
-            }
-        }
         
         private void FixedUpdate()
         {
@@ -81,10 +64,17 @@ namespace RehvidGames.Player
         private void PerformJump()
         {
             AddForceToRigidBody(Vector3.up * jumpForce, ForceMode.Impulse); 
-            onGround = false;
             animator.SetTrigger(AnimatorParameter.IsJumping);
         }
          
+        public void OnGroundChange(bool state)
+        {
+            if (onGround == state) return;
+            
+            onGround = state;
+            animator.SetBool(AnimatorParameter.OnGround, state);
+        }
+        
         public void OnMove(InputAction.CallbackContext context) => inputMovement = context.ReadValue<Vector2>();
         
         public void OnJump(InputAction.CallbackContext context) => isJumping = context.performed;
@@ -103,21 +93,7 @@ namespace RehvidGames.Player
             AddForceToRigidBody(force * dashForce, ForceMode.Impulse);
             animator.SetTrigger(AnimatorParameter.Dash);
         }
-
+        
         private void AddForceToRigidBody(Vector3 force, ForceMode mode) => rb.AddForce(force, mode);
-        
-        private void OnCollisionEnter(Collision other) => UpdateGroundState(other, true);
-        
-        private void OnCollisionExit(Collision other) => UpdateGroundState(other, false);
-        
-        private void UpdateGroundState(Collision collision, bool state)
-        {
-            if (!IsCollidingWithGround(collision)) return;
-            
-            onGround = state;
-            animator.SetBool(AnimatorParameter.OnGround, state);
-        }
-        
-        private bool IsCollidingWithGround(Collision collision) => ((1 << collision.gameObject.layer) & groundLayer) != 0; 
     }
 }
---
+++ Assets/Scripts/Utilities/GroundChecker.cs
@@ -0,0 +1,29 @@
+﻿namespace RehvidGames.Utilities
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class GroundChecker: MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private LayerMask groundLayer;
+        
+        [Header("Events")]
+        [SerializeField] private UnityEvent<bool> groundCheck;
+        
+        [Header("Debug")]
+        [SerializeField] private bool useDebug;
+        
+        private void OnCollisionEnter(Collision other) => UpdateGroundState(other, true);
+        private void OnCollisionExit(Collision other) => UpdateGroundState(other, false);
+        
+        private void UpdateGroundState(Collision collision, bool state)
+        {
+            if (!IsCollidingWithGround(collision)) return;
+            groundCheck?.Invoke(state);
+        }
+        
+        private bool IsCollidingWithGround(Collision collision) 
+            => ((1 << collision.gameObject.layer) & groundLayer) != 0; 
+    }
+}
