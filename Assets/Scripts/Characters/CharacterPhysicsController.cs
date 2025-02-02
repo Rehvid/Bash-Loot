c6f8d8c594879c4a9c4e49a0ff4ed04976f1b6ce
Dawid Harendarz <dharendarz2@gmail.com>
2025-02-02 10:58:09 +0100

    Add physics controller for characters

---
+++ Assets/Scripts/Characters/CharacterPhysicsController.cs
@@ -0,0 +1,15 @@
+﻿namespace RehvidGames.Characters
+{
+    using UnityEngine;
+
+    public class CharacterPhysicsController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rb; 
+        
+        public Vector2 LinearVelocity => rb.linearVelocity;
+        
+        public void AddForce(Vector2 force, ForceMode2D forceMode) => rb.AddForce(force, forceMode);
+        
+        public void SetLinearVelocity(Vector2 velocity) => rb.linearVelocity = velocity;
+    }
+}
