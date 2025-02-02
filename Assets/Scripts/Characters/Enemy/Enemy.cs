151b377be891267a31d319aa3584039ddc8eef56
Dawid Harendarz <dharendarz2@gmail.com>
2025-02-02 16:58:46 +0100

    Create enemy class and enum type

---
+++ Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -0,0 +1,13 @@
+﻿namespace RehvidGames.Characters.Enemy
+{
+    using Enums;
+    using UnityEngine;
+
+    public class Enemy : MonoBehaviour
+    {
+        
+        
+        [Header("Settings")]
+        [SerializeField] private EnemyType enemyType;
+    }
+}
