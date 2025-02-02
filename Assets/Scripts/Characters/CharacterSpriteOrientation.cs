096a9e1bebca2e8513072a24540a21e549461c39
Dawid Harendarz <dharendarz2@gmail.com>
2025-02-02 11:36:08 +0100

    Add sprite orientation for characters

---
+++ Assets/Scripts/Characters/CharacterSpriteOrientation.cs
@@ -0,0 +1,26 @@
+﻿namespace RehvidGames.Characters
+{
+    using UnityEngine;
+
+    public class CharacterSpriteOrientation: MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+
+        public void UpdateSpriteDirection(Vector2 inputMovement)
+        {
+            if (IsMovingHorizontally(inputMovement))
+            {
+                FlipHorizontally(inputMovement.x < 0);
+            }
+        }
+        
+        private bool IsMovingHorizontally(Vector2 inputMovement)
+        {
+            return !Mathf.Approximately(inputMovement.x, 0);
+        }
+        
+        private void FlipHorizontally(bool flip) => spriteRenderer.flipX = flip;
+        
+        public bool IsFlippedHorizontally() => spriteRenderer.flipX;
+    }
+}
