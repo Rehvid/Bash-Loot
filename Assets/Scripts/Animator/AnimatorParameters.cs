02d7e46bab603ac7d23f8e156214d39fb4ee47c2
Dawid Harendarz <dharendarz2@gmail.com>
2025-02-02 10:41:18 +0100

    Add animator parameters and state selector

---
+++ Assets/Scripts/Animator/AnimatorParameters.cs
@@ -0,0 +1,21 @@
+﻿namespace RehvidGames.Animator
+{
+    using UnityEngine;
+    
+    public static class MovementAnimatorParameters
+    {
+        public static readonly int XVelocity = Animator.StringToHash("xVelocity");
+        public static readonly int YVelocity = Animator.StringToHash("yVelocity");
+        public static readonly int OnGround = Animator.StringToHash("onGround");
+        public static readonly int Jump = Animator.StringToHash("jump");
+        public static readonly int Dash = Animator.StringToHash("dash");
+    }
+
+    public static class CombatAnimatorParameters
+    {
+        public static readonly int Attack = Animator.StringToHash("attack");
+        public static readonly int Block = Animator.StringToHash("block");
+        public static readonly int IsBlockingIdle = Animator.StringToHash("isBlockingIdle");
+        public static readonly int Roll = Animator.StringToHash("roll");
+    }
+}
---
+++ Assets/Scripts/Animator/RandomAnimationStateSelector.cs
@@ -0,0 +1,41 @@
+﻿namespace RehvidGames.Animator
+{
+    using UnityEngine;
+    
+    public class RandomAnimationStateSelector : StateMachineBehaviour
+    {
+        [SerializeField] private int count;
+        [SerializeField] private string parameterName;
+        
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            int newSelection = GetRandomSelection(animator);
+            animator.SetInteger(parameterName, newSelection);
+        }
+
+        private int GetRandomSelection(Animator animator)
+        {
+            int randomSelection = GenerateRandomSelection();
+            var currentSelection = animator.GetInteger(parameterName);
+            
+            if (count > 1)
+            {
+                randomSelection = EnsureDifferentSelection(randomSelection, currentSelection);
+            }
+
+            return randomSelection;
+        }
+
+        private int EnsureDifferentSelection(int selection, int currentSelection)
+        {
+            while (selection == currentSelection)
+            {
+                selection = GenerateRandomSelection();
+            }
+
+            return selection;
+        }
+        
+        private int GenerateRandomSelection() => Random.Range(0, count);
+    }
+}
