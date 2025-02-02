2816d50517ce274dadbd42de3f0e4421f43e7b06
Dawid Harendarz <dharendarz2@gmail.com>
2025-02-02 16:53:43 +0100

    Add cooldown timer

---
+++ Assets/Scripts/Utilities/CooldownTimer.cs
@@ -0,0 +1,20 @@
+﻿namespace RehvidGames.Utilities
+{
+    using UnityEngine;
+
+    public class CooldownTimer: MonoBehaviour
+    {
+        private float lastTime = -Mathf.Infinity; 
+        private float lastTimeFixed = -Mathf.Infinity;
+        
+        public void Reset() => lastTime = Time.time;
+        
+        public void ResetFixed() => lastTimeFixed = Time.fixedTime;
+        
+        public bool HasElapsed(float delay) => Time.time - lastTime >= delay;
+        
+        public bool HasElapsedFixed(float delay) => Time.fixedTime - lastTimeFixed >= delay;
+        
+        public bool IsInitializedFixed() => !float.IsNegativeInfinity(lastTimeFixed) && lastTimeFixed > 0.0f;
+    }
+}
