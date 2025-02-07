3c182868deeab5203f5951eb8d799b514104ec10
Dawid Harendarz <dharendarz2@gmail.com>
2025-02-07 13:42:01 +0100

    Add weapon class

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,33 @@
+﻿namespace RehvidGames.Weapon
+{
+    using Interfaces;
+    using UnityEngine;
+
+    public class Weapon : MonoBehaviour
+    {
+        [Header("Components")] 
+        [SerializeField] private Collider2D hitCollider;
+        
+        [Header("Settings")]
+        [SerializeField] private WeaponStats weaponStats;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(weaponStats.Damage);
+            }
+        }
+
+        public void ActivateAttackCollider()
+        {
+            hitCollider.isTrigger = true;
+        }
+
+        public void DeactivateAttackCollider()
+        {
+            hitCollider.isTrigger = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Weapon/WeaponStats.cs
@@ -0,0 +1,12 @@
+﻿namespace RehvidGames.Weapon
+{
+    using System;
+    using UnityEngine;
+
+    [Serializable]
+    public class WeaponStats
+    {
+        [Header("Stats")]
+        [field: SerializeField] public float Damage { get; private set; } = 10f;
+    }
+}
